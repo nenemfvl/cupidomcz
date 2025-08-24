be3ab92fb5e230bd572c55cf1b1f5e3af8bc59d1
nenemfvl <nenemfvl@gmail.com>
2025-08-24 01:40:28 -0300

    Corrige erro de digitação: createTransporter -> createTransport

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 // Configuração do transporter (Gmail)
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
